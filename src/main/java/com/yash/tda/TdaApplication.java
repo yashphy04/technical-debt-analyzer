3ec4db097d509cf647b41828be8a7c9b3cb6765b
Yash Raj <yashraj.phy@gmail.com>
2026-03-08 18:12:38 +0530

    update database configuration for Render PostgreSQL

--- src/main/java/com/yash/tda/TdaApplication.java
+++ src/main/java/com/yash/tda/TdaApplication.java
@@ -3,17 +3,9 @@ package com.yash.tda;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(
-		exclude = {
-				org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
-				org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
-				org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration.class
-		}
-)
+@SpringBootApplication
 public class TdaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TdaApplication.class, args);
 	}
-
 }
