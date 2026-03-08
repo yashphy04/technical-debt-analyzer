8cd94277caa41012c06de6264a55ed2341236d49
Yash Raj <yashraj.phy@gmail.com>
2026-03-08 15:57:05 +0530

    Fix repository scanning for Render deployment

--- src/main/java/com/yash/tda/TdaApplication.java
+++ src/main/java/com/yash/tda/TdaApplication.java
@@ -2,13 +2,14 @@ package com.yash.tda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableAsync
+@EnableJpaRepositories(basePackages = "com.yash.tda.repository")
 public class TdaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TdaApplication.class, args);
 	}
+
 }
